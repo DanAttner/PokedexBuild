718122e594a05a78b9016d7cce676aca0b55c69a
HarveyBirdman <56330163+Harvey-birdman@users.noreply.github.com>
2021-06-20 12:15:05 -0400

    created pokeList array and added first 3 pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
-alert('Hellow World');
+let pokemonList= [];
 
-let favoriteFood = "tator tots"
-document.write(favoriteFood)
+
+
+let bulbasaur = {
+    name:'Bulbasaur',
+    number: 1,
+    height: 0.7,
+    type: ['grass','poison']
+};
+
+let ivysaur = {
+    name:'Ivysaur',
+    number: 2,
+    height: 1,
+    type: ['grass','poison']
+};
+
+let  = venusaur = {
+    name:'Venusaur',
+    number: 3,
+    height: 2,
+    type: ['grass','poison']
+};
+
+pokemonList.push(bulbasaur, ivysaur, venusaur);
+
+console.log(pokemonList);
