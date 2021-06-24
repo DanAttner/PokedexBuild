7dc9bb941669b316e8f3fc61e43f38422af47045
HarveyBirdman <85701217+DanAttner@users.noreply.github.com>
2021-06-24 13:37:27 -0400

    changed back for 1.2, pokemon objects start in array instead of being pushed to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,23 @@
-let pokemonList= [];
-
-
-
-// createing objects
-let bulbasaur = {
-    name:'Bulbasaur',
-    number: 1,
-    height: 0.7,
-    type: ['grass','poison']
-};
-
-let ivysaur = {
-    name:'Ivysaur',
-    number: 2,
-    height: 1,
-    type: ['grass','poison']
-};
-
-let  = venusaur = {
-    name:'Venusaur',
-    number: 3,
-    height: 2,
-    type: ['grass','poison']
-};
-
-
-//pushing objects to array
-pokemonList.push(bulbasaur, ivysaur, venusaur);
-
-console.log(pokemonList);
-
-
-//loop for writeing out names and heights
-for (let i =0; i < pokemonList.length; i++){
-    
-    document.write(pokemonList[i].name + " " + pokemonList[i].height);
-
-    // if height is taller than 1, write something
-    if (pokemonList[i].height > 1){
-        document.write("  - Wow, that is a big pokemon!")
+let pokemonList= [
+    bulbasaur= {
+        name:'Bulbasaur',
+        number: 1,
+        height: 0.7,
+        type: ['grass','poison']
+    },
+    ivysaur = {
+        name:'Ivysaur',
+        number: 2,
+        height: 1,
+        type: ['grass','poison']
+    },
+    let  = venusaur = {
+        name:'Venusaur',
+        number: 3,
+        height: 2,
+        type: ['grass','poison']
     }
 
+];
 
-    document.write("<br>");
-
-
-
-};
+console.log(pokemonList)
