ff800bd85d1744fb33ee941541f81b940cd011da
HarveyBirdman <85701217+DanAttner@users.noreply.github.com>
2021-06-25 20:19:21 -0400

    1.2 commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 let pokemonList= [
-    bulbasaur= {
+    {
         name:'Bulbasaur',
         number: 1,
         height: 0.7,
         type: ['grass','poison']
     },
-    ivysaur = {
+    {
         name:'Ivysaur',
         number: 2,
         height: 1,
         type: ['grass','poison']
     },
-    let  = venusaur = {
+    {
         name:'Venusaur',
         number: 3,
         height: 2,
